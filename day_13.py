4dd045d9f187b985f7cf5f56a384a403ef2a8c15
Akash2king <akash637466@gmail.com>
2024-10-01 11:02:39 +0530

    roman to integer

--- day_13.py
+++ day_13.py
@@ -1,24 +1,22 @@
-class Solution:
-    def romanToInt(self, s: str) -> int:
-        row = {
-            "I":1,
-            "V":5,
-            "X":10,
-            "L":50,
-            "C":100,
-            "D":500,
-            "M":1000
-        }
-        res = 0
-        i = 0
-        for i ,j in zip(s,s[1:]):
-            if row[i] < row[j]:
-                res -= row[i]
-            else:
-                res += row[i]
 
-        return res + row[s[-1]]
-
+def romanToInt( s) :
+    row = {
+        "I":1,
+        "V":5,
+        "X":10,
+        "L":50,
+        "C":100,
+        "D":500,
+        "M":1000
+    }
+    res = 0
+    i = 0
+    for i ,j in zip(s,s[1:]):
+        if row[i] < row[j]:
+            res -= row[i]
+        else:
+            res += row[i]
+    return res + row[s[-1]]
 
 
         
